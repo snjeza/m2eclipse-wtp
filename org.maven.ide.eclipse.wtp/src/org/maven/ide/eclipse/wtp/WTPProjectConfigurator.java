68e451ddfd09c4ffa29e02496436aac4026da85e
ifedorenko <ifedorenko@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-04-30 17:31:05 +0000

    MNGECLIPSE-554: update j2ee module dependencies based on pom.xml changes

--- org.maven.ide.eclipse.wtp/src/org/maven/ide/eclipse/wtp/WTPProjectConfigurator.java
+++ org.maven.ide.eclipse.wtp/src/org/maven/ide/eclipse/wtp/WTPProjectConfigurator.java
@@ -8,13 +8,19 @@
 
 package org.maven.ide.eclipse.wtp;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.embedder.MavenEmbedder;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IClasspathAttribute;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
@@ -23,12 +29,20 @@ import org.eclipse.jst.common.project.facet.JavaFacetUtils;
 import org.eclipse.jst.j2ee.classpathdep.IClasspathDependencyConstants;
 import org.eclipse.jst.j2ee.web.project.facet.WebFacetInstallDataModelProvider;
 import org.eclipse.jst.j2ee.web.project.facet.WebFacetUtils;
+import org.eclipse.wst.common.componentcore.ComponentCore;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
+import org.eclipse.wst.common.componentcore.resources.IVirtualReference;
 import org.eclipse.wst.common.frameworks.datamodel.DataModelFactory;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wst.common.project.facet.core.IFacetedProject;
+import org.eclipse.wst.common.project.facet.core.IProjectFacet;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
+import org.maven.ide.eclipse.MavenPlugin;
 import org.maven.ide.eclipse.project.BuildPathManager;
+import org.maven.ide.eclipse.project.MavenProjectChangedEvent;
+import org.maven.ide.eclipse.project.MavenProjectFacade;
+import org.maven.ide.eclipse.project.MavenProjectManager;
 import org.maven.ide.eclipse.project.configurator.AbstractProjectConfigurator;
 import org.maven.ide.eclipse.project.configurator.ProjectConfigurationRequest;
 
@@ -37,6 +51,13 @@ import org.maven.ide.eclipse.project.configurator.ProjectConfigurationRequest;
  * @author Igor Fedorenko
  */
 public class WTPProjectConfigurator extends AbstractProjectConfigurator {
+  
+  private final MavenProjectManager projectManager;
+  
+  public WTPProjectConfigurator() {
+    MavenPlugin plugin = MavenPlugin.getDefault();
+    this.projectManager = plugin.getMavenProjectManager();
+  }
 
   @Override
   public void configure(MavenEmbedder embedder, ProjectConfigurationRequest request, IProgressMonitor monitor)
@@ -45,6 +66,51 @@ public class WTPProjectConfigurator extends AbstractProjectConfigurator {
     if(WarPluginConfiguration.isWarProject(mavenProject)) {
       IProject project = request.getProject();
       configureWtpWar(project, monitor);
+
+      // configure module dependencies
+      IVirtualComponent component = ComponentCore.createComponent(project);
+      
+      Set<IVirtualReference> references = new LinkedHashSet<IVirtualReference>();
+      for(MavenProjectFacade dependency : getWorkspaceDependencies(request.getProject(), request.getMavenProject())) {
+        configureWtpUtil(dependency.getProject(), monitor);
+        IVirtualComponent depComponent = ComponentCore.createComponent(dependency.getProject());
+        IVirtualReference reference = ComponentCore.createReference(component, depComponent);
+        reference.setRuntimePath(new Path("/WEB-INF/lib"));
+        references.add(reference);
+      }
+
+      // this wipes out all references configured manually via WTP UI
+      // XXX consider an option to add new references only
+      component.setReferences(references.toArray(new IVirtualReference[references.size()]));
+    }
+  }
+
+  private List<MavenProjectFacade> getWorkspaceDependencies(IProject project, MavenProject mavenProject) {
+    List<MavenProjectFacade> dependencies = new ArrayList<MavenProjectFacade>();
+    @SuppressWarnings("unchecked")
+    Set<Artifact> artifacts = mavenProject.getArtifacts();
+    for(Artifact artifact : artifacts) {
+      MavenProjectFacade dependency = projectManager.getMavenProject(artifact);
+      if (dependency != null 
+          && !dependency.getProject().equals(project)
+          && dependency.getFullPath(artifact.getFile()) != null)
+      {
+        dependencies.add(dependency);
+      }
+    }
+    return dependencies;
+  }
+
+  private void configureWtpUtil(IProject project, IProgressMonitor monitor) throws CoreException {
+    IFacetedProject facetedProject = ProjectFacetsManager.create(project, true, monitor);
+    IProjectFacetVersion javaFv = JavaFacetUtils.compilerLevelToFacet(JavaFacetUtils.getCompilerLevel(project));
+    if(!facetedProject.hasProjectFacet(javaFv)) {
+      facetedProject.installProjectFacet(javaFv, null, monitor);
+    }
+    
+    IProjectFacetVersion utilFv = UTILITY_FACET.getVersion("1.0");
+    if(!facetedProject.hasProjectFacet(utilFv)) {
+      facetedProject.installProjectFacet(utilFv, null, monitor);
     }
   }
 
@@ -81,6 +147,50 @@ public class WTPProjectConfigurator extends AbstractProjectConfigurator {
     javaProject.setRawClasspath(cp, monitor);
   }
 
-//	isWEB = facetedProject.hasProjectFacet(ProjectFacetsManager.getProjectFacet(IModuleConstants.JST_WEB_MODULE));
+  protected void mavenProjectChanged(MavenProjectChangedEvent event, IProgressMonitor monitor) throws CoreException {
+    MavenProjectFacade facade = event.getMavenProject();
+//    MavenProjectFacade oldFacade = event.getOldMavenProject();
+
+    IVirtualComponent component = ComponentCore.createComponent(facade.getProject());
+
+//    Set<IVirtualReference> oldReferences = new HashSet<IVirtualReference>();
+//    if (oldFacade != null) {
+//      for (MavenProjectFacade dependency : getWorkspaceDependencies(oldFacade.getProject(), oldFacade.getMavenProject())) {
+//        IVirtualComponent depComponent = ComponentCore.createComponent(dependency.getProject());
+//        if (depComponent != null) {
+//          IVirtualReference reference = component.getReference(depComponent.getName());
+//          if (reference != null) {
+//            oldReferences.add(reference);
+//          }
+//        }
+//      }
+//    }
+
+    Set<IVirtualReference> references = new LinkedHashSet<IVirtualReference>();
+    for (MavenProjectFacade dependency : getWorkspaceDependencies(facade.getProject(), facade.getMavenProject())) {
+      configureWtpUtil(dependency.getProject(), monitor);
+      IVirtualComponent depComponent = ComponentCore.createComponent(dependency.getProject());
+      IVirtualReference reference = ComponentCore.createReference(component, depComponent);
+      reference.setRuntimePath(new Path("/WEB-INF/lib"));
+//      oldReferences.remove(reference);
+      references.add(reference);
+    }
+
+//    for (IVirtualReference oldReference : oldReferences) {
+//      removeRefernce(component, oldReference);
+//    }
+    component.setReferences(references.toArray(new IVirtualReference[references.size()]));
+  }
+
+  //	isWEB = facetedProject.hasProjectFacet(ProjectFacetsManager.getProjectFacet(IModuleConstants.JST_WEB_MODULE));
+
+//  @SuppressWarnings("restriction")
+//  private void removeRefernce(IVirtualComponent component, IVirtualReference reference) {
+//    // no public API to remove references?
+//    ((VirtualComponent) component).removeReference(reference);
+//  }
+
+  // WTP 2.0 does not seem to have any public API to access Utility JAR facet
+  private static final IProjectFacet UTILITY_FACET = ProjectFacetsManager.getProjectFacet("jst.utility");
 
 }
